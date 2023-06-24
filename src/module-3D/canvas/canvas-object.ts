0637c96436459e67fb6a2aad2d332b82afee0688
carlosmab <carlosm.araujob@gmail.com>
2023-06-24 11:50:12 -0500

    Orbit controls

--- src/module-3D/canvas/canvas-object.ts
+++ src/module-3D/canvas/canvas-object.ts
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { Segment } from '../segments/segment';
-import { OrbitControls } from '../../jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'three-orbitcontrols-ts';
 
 export class CanvasObject {
   canvas: HTMLCanvasElement;
